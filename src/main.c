29c6b0fd9d65ca0855e517314abd6eeda770e231
Yi Yao <leaf_yao8621@hotmail.com>
2020-07-10 17:39:00 -0400

    Updates

--- src/main.c
+++ src/main.c
@@ -3,8 +3,8 @@
 
 int main(void) {
     struct Model m;
-    model_initialize(&m, 10, 5, 10, 1, 3, 1, 3);
-    for (int i = 0; i < 10; ++i) {
+    model_initialize(&m, 10, 0.1, 10, 1, 3, 1, 3);
+    for (int i = 0; i < 100; ++i) {
         model_update(&m);
         model_log(&m, stdout);
     }
