818d7474e0e24973cd8464f22777d699340a4eab
Yi Yao <leaf_yao8621@hotmail.com>
2020-07-10 17:18:56 -0400

    Updates

--- src/model/model.c
+++ src/model/model.c
@@ -11,9 +11,19 @@ static inline void entity_chase_acc(struct Entity *m, struct Entity *t) {
     double diff_x = t->loc_x - m->loc_x;
     double diff_y = t->loc_y - m->loc_y;
     double mag_sq = diff_x * diff_x + diff_y * diff_y;
-    double rat = sqrt(m->max_vel_sq / mag_sq);
+    double rat = sqrt(m->max_spd_sq / mag_sq);
     diff_x *= rat;
     diff_y *= rat;
+    double acc_x = diff_x - m->vel_x;
+    double acc_y = diff_y - m->vel_y;
+    mag_sq = acc_x * acc_x + acc_y * acc_y;
+    if (mag_sq > m->max_acc_sq) {
+        rat = sqrt(m->max_acc_sq / mag_sq);
+        acc_x *= rat;
+        acc_y *= rat;
+    }
+    m->acc_x = acc_x;
+    m->acc_y = acc_y;
 }
 
 static inline void entity_update_vel(struct Entity *e) {
@@ -50,41 +60,60 @@ int model_initialize(struct Model *m,
     if (!m) {
         return 1;
     }
-    m->stat = STAT_ONGOING;
+    
     if (width <= 0) {
         return 2;
     }
     m->width = width;
+
     if (height <= 0) {
         return 3;
     }
     m->height = height;
+
     if (blast_radius <= 0) {
         return 4;
     }
     m->blast_radius = blast_radius;
     m->blast_rasius_sq = blast_radius * blast_radius;
+
     mt19937_initialize(&m->gen, seed);
+
     if (target_max_acc <= 0) {
         return 5;
     }
     m->target.max_acc = target_max_acc;
     m->target.max_acc_sq = target_max_acc * target_max_acc;
+
     if (target_max_spd <= 0) {
         return 6;
     }
     m->target.max_spd = target_max_spd;
     m->target.max_spd_sq = target_max_spd * target_max_spd;
+
     if (missle_max_acc <= 0) {
         return 7;
     }
     m->missle.max_acc = missle_max_acc;
     m->missle.max_acc_sq = missle_max_acc * missle_max_acc;
+
     if (missle_max_spd <= 0) {
         return 8;
     }
     m->missle.max_spd = missle_max_spd;
     m->missle.max_spd_sq = missle_max_spd * missle_max_spd;
+    
+    m->stat = STAT_ONGOING;
+    
+    m->target.acc_x = 0;
+    m->target.acc_y = 0;
+    m->target.vel_x = 0;
+    m->target.vel_y = 0;
+
+    m->missle.acc_x = 0;
+    m->missle.acc_y = 0;
+    m->missle.vel_x = 0;
+    m->missle.vel_y = 0;
     return 0;
 }
 
@@ -100,7 +129,7 @@ int model_update(struct Model *m) {
         m->stat = STAT_FAILURE;
         return 0;
     }
-    entity_update_acc(&m->missle, &m->gen);
+    entity_chase_acc(&m->missle, &m->gen);
     entity_update_vel(&m->missle);
     entity_update_loc(&m->missle);
     if (m->missle.loc_x < 0 || m->missle.loc_x >= m->width ||
