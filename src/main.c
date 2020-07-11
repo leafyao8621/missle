603e516971cc107ec92a5f352344872b6fb2e372
Yi Yao <leaf_yao8621@hotmail.com>
2020-07-11 18:00:20 -0400

    Updates

--- src/main.c
+++ src/main.c
@@ -4,8 +4,8 @@ int main(void) {
     struct Engine engine;
     double prob;
     char success;
-    engine_initialize(&engine, 12, 100, 100, 10, 3, 10, 10, 1, 3, 1, 3);
-    engine_run(&engine, &prob, 1, stdout);
+    engine_initialize(&engine, 12, 1000000, 100, 10, 3, 10, 10, 1, 3, 1, 3);
+    engine_run(&engine, &prob, 0, 0);
     printf("prob: %lf\n", prob);
     return 0;
 }
